23f99f091c8b1fd49dfb77adcbcccbc59b16fcb0
chengeng <chengeng@bloggeng.com>
2020-07-02 11:29:37 +0800

    修改内容

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const { Wechaty } = require('wechaty');
 const { PuppetPadplus } = require('wechaty-puppet-padplus');
 const WechatyWebPanelPlugin = require('wechaty-web-panel');
-const token = '';
-const puppet = new PuppetPadplus({
-  token,
-});
 const name = 'wechat-assistant-pro';
-const bot = new Wechaty({
-  puppet,
-  name, // generate xxxx.memory-card.json and save login data for the next login
-});
+const token = '';
+let bot = '';
+if (token) {
+  const puppet = new PuppetPadplus({
+    token,
+  });
+  bot = new Wechaty({
+    puppet,
+    name, // generate xxxx.memory-card.json and save login data for the next login
+  });
+} else {
+  bot = new Wechaty({
+    name, // generate xxxx.memory-card.json and save login data for the next login
+  });
+}
 
 bot
   .use(WechatyWebPanelPlugin())
