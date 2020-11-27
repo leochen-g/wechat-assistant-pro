f0877575c86d0c599b86939da1baf2d91cb0ad06
chengeng <chengeng@bloggeng.com>
2020-11-27 09:40:13 +0800

    fix bug

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
 const { Wechaty } = require('wechaty');
-const { PuppetPadplus } = require('wechaty-puppet-padplus');
 const WechatyWebPanelPlugin = require('wechaty-web-panel');
 const name = 'wechat-assistant-pro';
 const token = '';
 let bot = '';
-if (token) {
-  const puppet = new PuppetPadplus({
-    token,
-  });
-  bot = new Wechaty({
-    puppet,
-    name, // generate xxxx.memory-card.json and save login data for the next login
-  });
-} else {
-  bot = new Wechaty({
-    name, // generate xxxx.memory-card.json and save login data for the next login
-  });
-}
+bot = new Wechaty({
+  name, // generate xxxx.memory-card.json and save login data for the next login
+});
 
 bot
   .use(WechatyWebPanelPlugin())
