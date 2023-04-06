0404b8a29be61dc61dc726dcdeceae09a8d3ff45
chengeng <chengeng@bloggeng.com>
2023-04-06 19:45:56 +0800

    docs(模块): 更新了一下文档

--- src/office.js
+++ src/office.js
@@ -7,6 +7,7 @@ const oa = new PuppetOA({
     appId           : '公众号appid',
     appSecret       : '公众号appSecret',
     token           : '公众号加密token',
+    // personalMode: true, // 如果你是个人订阅号或者未认证 请开启此项
     // port 和 webhookProxyUrl 自己选择一个
     // port: 8077, // 有自己域名或者服务器 可以启用这个 服务启动的端口 自己映射好配到公众号后台机就行
     webhookProxyUrl: 'https://****.loca.lt'  // 如果没有自己的域名可以直接用默认自带穿透代理服务localtunnel ***替换成随机字符串即可  这个域名记得配置到公众号后台
