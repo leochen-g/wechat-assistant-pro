7fee431ab9d4e6c6e64fcb11a2e6ed7b1370e978
chengeng <chengeng@bloggeng.com>
2023-05-08 18:46:58 +0800

    feat(模块): 添加企微支持

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import {WechatyWebPanelPlugin} from 'wechaty-web-panel'
 const name = 'wechat-assistant-pro';
 let bot = '';
 let padLocalToken = '' // 如果申请了ipadlocal的token,可以直接填入
+let workProToken = '' // 如果申请了企业微信的token 可以直接填入
 
 if (process.env['PAD_LOCAL_TOKEN']) {
     console.log('读取到环境变量中的ipadLocalToken')
     padLocalToken = process.env['PAD_LOCAL_TOKEN']
+}
+
+if (process.env['WORK_PRO_TOKEN']) {
+    console.log('读取到环境变量中的企微token')
+    workProToken = process.env['WORK_PRO_TOKEN']
 }
 
 if (padLocalToken) {
@@ -19,6 +25,16 @@ if (padLocalToken) {
         }, puppet: 'wechaty-puppet-padlocal',
     });
 
+} else if(workProToken) {
+    console.log('读取到环境变量中的企微 token 使用企业微信协议启动')
+    bot = WechatyBuilder.build({
+        name, // generate xxxx.memory-card.json and save login data for the next login
+        puppet: 'wechaty-puppet-service',
+        puppetOptions: {
+            tls: { disable: true },
+            token: workProToken
+        },
+    });
 } else {
     console.log('默认使用wechat4u协议启动')
     bot = WechatyBuilder.build({
